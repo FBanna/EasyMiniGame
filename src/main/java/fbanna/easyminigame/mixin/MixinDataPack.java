ace210c64a7687ee55c06c0f577acf692136c179
FBanna <findlaybannatyne@gmail.com>
2025-01-03 09:19:53 +0000

    mixin

---
+++ src/main/java/fbanna/easyminigame/mixin/MixinDataPack.java
@@ -0,0 +1,30 @@
+package fbanna.easyminigame.mixin;
+
+
+import net.minecraft.resource.*;
+import net.minecraft.util.path.SymlinkFinder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.nio.file.Path;
+
+@Mixin(VanillaDataPackProvider.class)
+public class MixinDataPack {
+
+
+    @Inject(method = "createManager(Ljava/nio/file/Path;Lnet/minecraft/util/path/SymlinkFinder;)Lnet/minecraft/resource/ResourcePackManager;", at = @At(value = "HEAD"), cancellable = true)
+    private static void inject(Path dataPacksPath, SymlinkFinder symlinkFinder, CallbackInfoReturnable<ResourcePackManager> cir) {
+        cir.setReturnValue(
+
+                new ResourcePackManager(
+                        new VanillaDataPackProvider(symlinkFinder),
+                        new FileResourcePackProvider(dataPacksPath, ResourceType.SERVER_DATA, ResourcePackSource.WORLD, symlinkFinder),
+                        new FileResourcePackProvider(Path.of("./easyminigame/datapacktest/"), ResourceType.SERVER_DATA, ResourcePackSource.WORLD, symlinkFinder)
+                )
+
+        );
+    }
+
+}
